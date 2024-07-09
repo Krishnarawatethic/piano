3f4a03760525ee71e773f9e8c7b4e7b012cfa41e
Krishna Rawat <160142827+Krishnarawatethic@users.noreply.github.com>
2024-07-09 18:00:08 +0530

    Update script.js
    Added multiple keys play at once funtionality

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
 let pianoContainer = document.getElementsByClassName("piano-container");
 const base = "./audio/";
+
 window.onload = () => {
-  //24keys
-  for (let index = 1; index <= 24; index++) {
-    let div = document.createElement("div");
-    div.classList.add("key", index <= 10 ? "black-key" : "white-key");
-    //For playing audio on click
-    const number = index <= 9 ? "0" + index : index;
-    div.addEventListener("click", () => {
-      new Audio(`${base}key${number}.mp3`).play();
-    });
-    pianoContainer[0].appendChild(div);
-  }
+    let keysPressed = new Set(); 
+    for (let index = 1; index <= 24; index++) {
+        let div = document.createElement("div");
+        div.classList.add("key", index <= 10 ? "black-key" : "white-key");
+        
+        const number = index <= 9 ? "0" + index : index;
+        
+      
+        div.addEventListener("mousedown", () => {
+            keysPressed.add(number);
+            playChord(keysPressed);
+        });
+
+        div.addEventListener("mouseup", () => {
+            keysPressed.delete(number);
+            stopChord();
+        });
+
+        div.addEventListener("mouseleave", () => {
+            keysPressed.delete(number);
+            stopChord();
+        });
+
+        pianoContainer[0].appendChild(div);
+    }
+
+ 
+    function playChord(keys) {
+        keys.forEach(key => {
+            new Audio(`${base}key${key}.mp3`).play();
+        });
+    }
+
+    
+    function stopChord() {
+        
+        let sounds = document.getElementsByTagName('audio');
+        for(let i = 0; i < sounds.length; i++) {
+            sounds[i].pause();
+            sounds[i].currentTime = 0;
+        }
+    }
 };
